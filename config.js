89391a9b009afb1e07378caf4c603a59c04e119d
tuenh2004 <150781556+tuenh2004@users.noreply.github.com>
2024-10-20 12:49:43 +0700

    gui-thao

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '❤';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Click To Open';
+const titleCard = 'Gửi Thu Thảo';
+const contentCard = 'Chúc Thu Thảo 20-10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau From T';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thuthao.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
